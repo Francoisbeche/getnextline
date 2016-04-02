00cea2bc18222cc527050e83d10f78cdf5d1e7b3
beche_f <francois.beche@epitech.eu>
2016-04-02 22:37:30 +0000

    ol

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/*
+** get_next_line.h for  in /home/beche_f/rendu/CPE/get_next_line
+**
+** Made by beche_f
+** Login   <beche_f@epitech.net>
+**
+** Started on  Sun Jan 10 19:59:09 2016 beche_f
+** Last update Thu Jan 14 23:33:27 2016 beche_f
+*/
+
+#ifndef GET_NEXT_LINE_H_
+# define GET_NEXT_LINE_H_
+
+# ifndef READ_SIZE
+#  define READ_SIZE (230)
+# endif /* !READ_SIZE */
+
+char	*get_next_line(int);
+
+# endif /* GET_NEXT_LINE_H_ */
